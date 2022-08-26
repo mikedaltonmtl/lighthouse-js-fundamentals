4cf88cf1992e8adf264d2d4d7c315bd9ade7a3a4
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-08-26 19:22:52 +0000

    even.js

---
+++ even.js
@@ -0,0 +1,8 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+// const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+
+console.log("10 " + isEven(10));
+console.log("11 " + isEven(11));
